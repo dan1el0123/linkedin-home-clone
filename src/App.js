64b9f4e0cc384c691bb6e1e58a47e3de9815c2b7
Daniel <conevskidaniel1999@gmail.com>
2022-08-24 11:49:46 +0200

    add register functionality

--- src/App.js
+++ src/App.js
@@ -3,11 +3,33 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Login from "./Login";
-import { useSelector } from "react-redux";
-import { selectUser } from "./features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import { useEffect } from "react";
+import { auth } from "./firebase";
 
 function App() {
     const user = useSelector(selectUser);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        auth.onAuthStateChanged((userAuth) => {
+            if (userAuth) {
+                // user is logged in
+                dispatch(
+                    login({
+                        email: userAuth.email,
+                        uid: userAuth.uid,
+                        displayName: userAuth.displayName,
+                        photoUrl: userAuth.photoURL,
+                    })
+                );
+            } else {
+                // user is logged out
+                dispatch(logout());
+            }
+        });
+    }, [dispatch]);
 
     return (
         <div className="app">
--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,90 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "./features/userSlice";
+import { auth } from "./firebase";
+import "./Login.css";
+
 function Login() {
-    return <div>Login</div>;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [profilePic, setProfilePic] = useState("");
+    const dispatch = useDispatch();
+
+    const loginToApp = (e) => {
+        e.preventDefault();
+    };
+    const register = () => {
+        if (!name) {
+            return alert("Please enter a full name");
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((userAuth) => {
+                userAuth.user
+                    .updateProfile({
+                        displayName: name,
+                        photoURL: profilePic,
+                    })
+                    .then(() => {
+                        dispatch(
+                            login({
+                                email: userAuth.user.email,
+                                uid: userAuth.user.uid,
+                                displayName: name,
+                                photoUrl: profilePic,
+                            })
+                        );
+                    });
+            })
+            .catch((error) => alert(error));
+    };
+
+    return (
+        <div className="login">
+            <img
+                src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks"
+                alt=""
+            />
+
+            <form>
+                <input
+                    type="text"
+                    placeholder="Full name (required if registering)"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Profile Picture URL (optional)"
+                    value={profilePic}
+                    onChange={(e) => setProfilePic(e.target.value)}
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit" onClick={loginToApp}>
+                    Sign In
+                </button>
+            </form>
+
+            <p>
+                Not a member?{" "}
+                <span className="login__register" onClick={register}>
+                    Register Now
+                </span>
+            </p>
+        </div>
+    );
 }
 
 export default Login;
